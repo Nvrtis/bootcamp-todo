0ca40ecb699620193144b70ea68a724e5e3fbcec
Nicholas Vrtis <nvav91@gmail.com>
2020-11-21 20:18:19 +0100

    renderd the todos

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
-renderTodos();
-
 function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
-
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-
-    var li = document.createElement("li");
-    li.textContent = todo;
-    todoList.appendChild(li);
+  for(var i = 0; i < todos.length; i++) {
+    // create li element
+    var li = document.createElement('li')
+    //  add todo text to li
+    li.innerText = todos[i]
+    // append li to todoList
+    todoList.appendChild(li)
   }
 }
+
+renderTodos()
